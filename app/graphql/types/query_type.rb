3caf71c903818cd03cc77656441ef9671c1eb4ac
Joseph Oluoch <ochiengolu@gmail.com>
2024-03-21 00:16:32 +0300

    graphql setup

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,39 +1,19 @@
-# frozen_string_literal: true
+# app/graphql/types/query_type.rb
 
 module Types
-  class QueryType < Types::BaseObject
-    field :node, Types::NodeType, null: true, description: "Fetches an object given its ID." do
-      argument :id, ID, required: true, description: "ID of the object."
+  class QueryType < GraphQL::Schema::Object
+    field :users, [Types::UserType], null: true, description: "Get all users"
+
+    # Resolver for fetching all users
+    def users
+      Resolvers::UsersResolver.call(nil, nil, nil)
     end
 
-    def node(id:)
-      context.schema.object_from_id(id, context)
+    field :posts, [Types::PostType], null: true, description: "Get all posts"
+
+    # Resolver for fetching all posts
+    def posts
+      Resolvers::PostsResolver.call(nil, nil, nil)
     end
-
-    field :nodes, [Types::NodeType, null: true], null: true, description: "Fetches a list of objects given a list of IDs." do
-      argument :ids, [ID], required: true, description: "IDs of the objects."
-    end
-
-    def nodes(ids:)
-      ids.map { |id| context.schema.object_from_id(id, context) }
-    end
-
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
-    end
-  
-      field :users, [Types::UserType], null: false, resolve: Resolvers::UsersResolver
-      field :posts, [Types::PostType], null: false, resolve: Resolvers::PostsResolver
-    
-
-
   end
-
-  
 end
